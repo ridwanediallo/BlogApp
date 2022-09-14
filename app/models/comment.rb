3c2ade2225ee6d14a250aaaf3967209a04d20125
ridwan <ridwanediallo99@gmail.com>
2022-09-14 18:51:02 +0000

    Add associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ApplicationRecord
+    belongs_to :user, class_name: "user", foreign_key: "user_id"
+    has_many :posts, class_name: "post", foreign_key: "postId"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :comments, class_name: "comment", foreign_key: "authorId"
+    has_many :likes, class_name: "like", foreign_key: "authorId"
+    has_many :posts, class_name: "post", foreign_key: "authorId"
 end
