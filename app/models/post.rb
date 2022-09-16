bcd782d0e6c4f480cab7c3db02ea7741a89e0d75
ridwan <ridwanediallo99@gmail.com>
2022-09-16 19:44:03 +0000

    Add Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
-    belongs_to :user, class_name: "user", foreign_key: "user_id"
-    has_many :posts, class_name: "post", foreign_key: "postId"
+  belongs_to :author, class_name: "User", foreign_key: "author_id"
+  has_many :likes
+  has_many :comments
+
+  def last_five_comments
+    comments.last(5)
+  end
+
+  def update_posts_counter counter
+    author.postsCounter = counter
+    save
+  end
 end
