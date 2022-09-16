8817735f131a281a6772c22a3751169967ad00fd
ridwan <ridwanediallo99@gmail.com>
2022-09-16 19:44:25 +0000

    Add User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    has_many :comments, class_name: "comment", foreign_key: "authorId"
-    has_many :likes, class_name: "like", foreign_key: "authorId"
-    has_many :posts, class_name: "post", foreign_key: "authorId"
+  has_many :comments, foreign_key: "author_id"
+  has_many :likes, foreign_key: "author_id"
+  has_many :posts, foreign_key: "author_id"
+
+  def last_three_posts
+    posts.last(3)
+  end
 end
