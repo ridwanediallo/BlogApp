2776f73551ff607831e08a1af751532c4065b800
ridwan <ridwanediallo99@gmail.com>
2022-09-16 19:43:35 +0000

    Add Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,9 @@
 class Like < ApplicationRecord
-    belongs_to :user, class_name: "user", foreign_key: "user_id"
+  belongs_to :author, class_name: "User", foreign_key: "author_id"
+  belongs_to :post
+
+  def update_likes_counter(counter)
+    post.likesCounter = counter
+    save
+  end
 end
