881aa8dd5614fe0cc221b5af30181950cde790bd
ridwan <ridwanediallo99@gmail.com>
2022-09-17 14:15:07 +0000

    Fix linters

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-    subject { Post.new(title: 'Hello poeple', text: 'Everybody come on he is waking up.', commentsCounter: 5,  likesCounter: 10) }
+  subject do
+    Post.new(title: 'Hello poeple', text: 'Everybody come on he is waking up.', commentsCounter: 5, likesCounter: 10)
+  end
 
-    before { subject.save }
+  before { subject.save }
 
-    it 'title should be present' do
-        subject.title = nil
-        expect(subject).to_not be_valid
-    end
+  it 'title should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
 
-    it 'title should be present' do
-        subject.text = 'text' * 250
-        expect(subject).to_not be_valid
-    end
+  it 'title should be present' do
+    subject.text = 'text' * 250
+    expect(subject).to_not be_valid
+  end
 
-    it 'commentsCounter should be an integer' do
-        subject.commentsCounter = '20'
-        expect(subject).to_not be_valid
-    end
+  it 'commentsCounter should be an integer' do
+    subject.commentsCounter = '20'
+    expect(subject).to_not be_valid
+  end
 
-    it 'likesCounter should be an integer' do
-        subject.likesCounter = '100'
-        expect(subject).to_not be_valid
-    end
+  it 'likesCounter should be an integer' do
+    subject.likesCounter = '100'
+    expect(subject).to_not be_valid
+  end
 
-    it 'commentsCounter should be 5' do
-        expect(subject.commentsCounter).to be(5)
-    end
+  it 'commentsCounter should be 5' do
+    expect(subject.commentsCounter).to be(5)
+  end
 
-    it 'likesCounter should be 10' do
-        expect(subject.likesCounter).to be(10)
-    end
-
+  it 'likesCounter should be 10' do
+    expect(subject.likesCounter).to be(10)
+  end
 end
-
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    subject { User.new(name: 'Ridwan', photo: 'ridwane.png', bio: 'A software developer', postsCounter: 50 ) }
+  subject { User.new(name: 'Ridwan', photo: 'ridwane.png', bio: 'A software developer', postsCounter: 50) }
 
-    before { subject.save }
+  before { subject.save }
 
-    it 'title should be present' do
-        subject.name = nil
-        expect(subject).to_not be_valid
-    end
+  it 'title should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 
-    it 'postsCounter should be an integer' do
-        subject.postsCounter = 'hello'
-        expect(subject).to_not be_valid
-    end
+  it 'postsCounter should be an integer' do
+    subject.postsCounter = 'hello'
+    expect(subject).to_not be_valid
+  end
 
-    it 'postsCounter should be 50' do
-        expect(subject.postsCounter).to be(50)
-    end
-
+  it 'postsCounter should be 50' do
+    expect(subject.postsCounter).to be(50)
+  end
 end
-
