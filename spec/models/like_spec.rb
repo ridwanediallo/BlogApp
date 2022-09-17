b4ca459da9a6d75f3613cb6b858cd75648fd7c64
ridwan <ridwanediallo99@gmail.com>
2022-09-17 14:13:50 +0000

    Add rspec

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,18 @@
+
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  describe 'Tests for Like model' do
+    first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                             bio: 'Teacher from Mexico.')
+    first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+    subject { Like.new }
+    before { subject.save }
+
+    it 'Test if likesCounter is getting updated' do
+      first_post.likesCounter = 2
+      subject.post = first_post
+      expect(subject.post.likesCounter).to eq 2
+    end
+  end
+end
