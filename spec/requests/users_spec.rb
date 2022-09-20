fed934e06b3b1acafd7a68b99ef620daeb1c74ce
ridwan <ridwanediallo99@gmail.com>
2022-09-20 19:55:04 +0000

    Fix Linters

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,24 +6,24 @@ RSpec.describe 'Users', type: :request do
       get users_path
     end
 
-   it "is a success" do
-     expect(response).to have_http_status(:ok)
-   end
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
 
-   it "renders 'index' template" do
-     expect(response).to render_template('index')
-   end
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
 
-   it "response body should includes correct placeholder text." do
-     expect(response.body).to include('Users list')
-   end
+    it 'response body should includes correct placeholder text.' do
+      expect(response.body).to include('Users list')
+    end
   end
   describe 'GET /show' do
     before :each do
       get users_path
     end
-   it "is a success" do
-     expect(response).to have_http_status(200)
-   end
+    it 'is a success' do
+      expect(response).to have_http_status(200)
+    end
   end
 end
