17dedbf24f147a42a9075975f6911d4d3cf68233
ridwan <ridwanediallo99@gmail.com>
2022-09-20 19:52:45 +0000

    Add tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,24 @@
+
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    context 'when the page is opened' do
+      it 'return a correct response' do
+        get '/users', params: { id: 1 }
+
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe 'GET /show' do
+    context 'when the page is opened' do
+      it 'returns the correct response' do
+        get '/users', params: { id: 1 }
+
+        expect(response).to have_http_status(200)
+      end
+    end
   end
 end
