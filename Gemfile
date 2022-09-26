6752c50474c59c1478d70fa8b6395008c203d9c1
ridwan <ridwanediallo99@gmail.com>
2022-09-26 22:29:08 +0000

    Fix linters

--- Gemfile
+++ Gemfile
@@ -52,7 +52,7 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
 
-  gem "database_cleaner"
+  gem 'database_cleaner'
   gem 'rspec-rails'
 end
 
@@ -70,7 +70,6 @@ end
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem 'capybara'
-  gem "selenium-webdriver"
   gem 'rails-controller-testing'
   gem 'selenium-webdriver'
   gem 'webdrivers'
--- spec/views/user.index_spec.rb
+++ spec/views/user.index_spec.rb
@@ -1,32 +1,32 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "User", type: :system do
-  describe "index page" do
+RSpec.describe 'User', type: :system do
+  describe 'index page' do
     before(:each) do
       @user =
         User.create(
-          name: "Tom",
-          bio: "Software Developer",
-          photo: "https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+          name: 'Tom',
+          bio: 'Software Developer',
+          photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
           postsCounter: 4
         )
     end
-    it "I can see the username of all other users" do
+    it 'I can see the username of all other users' do
       visit users_index_path
-      expect(page).to have_content("Tom")
+      expect(page).to have_content('Tom')
     end
 
-    it "I can see the profile picture for each user." do
+    it 'I can see the profile picture for each user.' do
       visit users_index_path
       expect(page).to have_css("img[src*='https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80']")
     end
 
-     it "I can see the number of posts each user has written." do
+    it 'I can see the number of posts each user has written.' do
       visit users_index_path
       expect(page).to have_content(0)
     end
 
-     it "When I click on a user, I am redirected to that user's show page." do
+    it "When I click on a user, I am redirected to that user's show page." do
       visit user_path(@user.id)
       expect(page).to have_content('Tom')
     end
