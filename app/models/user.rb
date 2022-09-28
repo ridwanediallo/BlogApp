29674ee57e441d33056ed80f67114790a986bc84
Felix Ouma <fatonoh@gmail.com>
2022-09-28 21:21:12 +0300

    Add confirm user email on registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   validates :postsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_initialize :init
+  before_save :default_values
 
   def last_three_posts
     posts.order('created_at Desc').limit(3)
@@ -19,5 +20,9 @@ class User < ApplicationRecord
   def init
     self.postsCounter ||= 0
     true
+  end
+
+  def default_values
+    self.photo = 'https://img.myloview.com/posters/social-media-user-icon-default-avatar-profile-image-400-251200036.jpg' if self.photo.nil?
   end
 end
